ff850c377d340432542f6a19e4d2840447ede84c
looper <m17701848923@163.com>
2017-12-04 15:38:40 +0800

    定时同步机制

--- src/main/java/com/quark/job/scheduler/MySimpleJob.java
+++ src/main/java/com/quark/job/scheduler/MySimpleJob.java
@@ -13,6 +13,7 @@ public class MySimpleJob implements SimpleJob {
     @Override
     public void execute(ShardingContext context) {
         //一个job中可以进行分片   （相当于一个个小job）
+        //每一个分片根据策略来指定运行的作业服务器上 （具体如何分配根据策略来指定）
         switch (context.getShardingItem()) {
             //case的数量必须和sharding-item-parameters 对应
             case 0:
